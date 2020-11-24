cf46188dc6663e7f031d2d12b705e37fc6862809
Elena <kolomeets.elena7@gmail.com>
2020-11-24 12:21:53 +0100

    Scan '.*' files, auto-generate secret_word list
    Modify main() func: one block for scanning with or w/o gitignore files,
    add block for scanning files starting with dot ('.');
    auto-generate secret_words list on top of the func, extend it;
    save all paths as relpath to user_path;
    if files cannot be open raise general Exception.

--- secret_scanner.py
+++ secret_scanner.py
@@ -12,7 +12,7 @@ parser = argparse.ArgumentParser(
                 'After scanning a summary of the results is given'
                 'in "secret_scanner_results.txt" file (generated in the scanned dir and added to .gitignore) '
                 'and in the terminal (if run in the terminal).',
-    epilog='Thank you for using Secret Scanner! Hope your secrets are safe now.'
+    epilog='Thank you for using Secret Scanner! Hopefully your secrets are safe now.'
            'The author is Elena Kolomeets (GitHub elena-kolomeets).',
 )
 parser.add_argument(
@@ -20,7 +20,7 @@ parser.add_argument(
     type=str,
     default='.',
     required=False,
-    help='Provide the path to the directory that needs to be scanned for secrets. '
+    help='Provide the absolute or relative path to the directory that needs to be scanned for secrets. '
          'By default the current directory is used.',
     metavar='[path]',
 )
@@ -33,16 +33,13 @@ parser.add_argument(
 
 args = parser.parse_args()
 
-# ADD generate txt file and add it to .gitignore
-
 
 def ignore(user_path):
     """
     Function that checks if dir contains .gitignore
     and returns a list of all ignored files from all subdirs
     """
-    gitignore = []
-    ignore_list = []
+    gitignore, ignore_list = [], []
     if os.path.exists(user_path + '/.gitignore'):
         with open(user_path + '/.gitignore') as g:
             for line in g:
@@ -50,68 +47,76 @@ def ignore(user_path):
                     line = line[:-1]
                 if line[-1] == '/':
                     line = line + '*'
+                if line[:2] == '**':
+                    line = line[2:]
                 gitignore.append(line)
         # print(gitignore)
         for i in gitignore:
-            i = user_path + '/' + i + '*'
+            i = user_path + '/**/' + i + '*'
             for j in glob.glob(i, recursive=True):
-                ignore_list.append(os.path.normpath(j))
-        # print(ignore_list, len(ignore_list))
+                ignore_list.append(os.path.relpath(j, start=user_path))
+        # print(ignore_list)
     return ignore_list
 
 
 def main(user_path, all_files):
-    secret_words = ['password:', 'PASSWORD:', 'Password:', 'password :', 'PASSWORD :', 'Password :',
-                    'password =', 'PASSWORD =', 'Password =', 'password=', 'PASSWORD=', 'Password=',
-                    'key:', 'KEY:', 'Key:', 'key :', 'KEY :', 'Key :',
-                    'key =', 'KEY =', 'Key =', 'key=', 'KEY=', 'Key=',
-    ]
     os.path.normpath(user_path)
     if os.path.isdir(user_path):
+        # generate a list of secret words to scan for
+        words_case, secret_words = [], []
+        for word in ['password', 'pass', 'key', 'credential', 'database_url', 'database-url', 'db_url', 'db-url']:
+            words_case.extend((word, word.upper(), word.capitalize()))
+        for word in words_case:
+            secret_words.extend((word + ':', word + ' :', word + '=', word + ' =', word + ' is'))
+
+        # check if --all flag is used (to include all .gitignore files)
         # get a list of files added to .gitignore (empty list if no .gitignore) to exclude them from scanning
-        scan_ignore = ignore(user_path)
+        if not all_files:
+            scan_ignore = ignore(user_path)
+        else:
+            scan_ignore = []
 
         # scanning the folder for secrets: adding file names
         # and their lines with secrets to list of dictionaries
-        file_list = []
-        not_open = []
+        file_list, not_open = [], []
+
+        # scanning for files starting with '.' as they are not matched by default with glob.glob
+        for dot_name in glob.glob(user_path+'/**/.*', recursive=True):
+            if os.path.relpath(dot_name, start=user_path) in scan_ignore:
+                continue
+            try:
+                with open(dot_name, mode='r', encoding='utf-8') as df:
+                    for dfile_line in df:
+                        for secret_word in secret_words:
+                            if secret_word in dfile_line:
+                                file_list.append({'file_name': os.path.relpath(dot_name, start=user_path),
+                                                  'file_line': dfile_line})
+            except Exception:
+                # creating the list of files that could not be open
+                not_open.append(f"Could not open the file {os.path.relpath(dot_name, start=user_path)}")
+                continue
+
+        # scanning the rest of the files
         for name in glob.glob(user_path+'/**/*', recursive=True):
-            # check if --all flag is missing (to exclude all .gitignore files)
-            if not all_files:
-                if os.path.isdir(name) or os.path.normpath(name) in scan_ignore:
-                    continue
-                try:
-                    with open(name, mode='r', encoding='utf-8') as f:
-                        for file_line in f:
-                            for word in secret_words:
-                                if word in file_line:
-                                    file_list.append({'file_name': os.path.relpath(name, start=user_path),
-                                                      'file_content': file_line})
-                except UnicodeDecodeError:
-                    # creating the list of files that could not be open
-                    not_open.append(f"Could not open the file {os.path.relpath(name, start=user_path)}")
-                    continue
-            # if --all flag is used (to scan .gitignore files as well)
-            else:
-                if os.path.isdir(name):
-                    continue
-                try:
-                    with open(name, mode='r', encoding='utf-8') as f:
-                        for file_line in f:
-                            for word in secret_words:
-                                if word in file_line:
-                                    file_list.append({'file_name': os.path.relpath(name, start=user_path),
-                                                      'file_content': file_line})
-                except UnicodeDecodeError:
-                    # creating the list of files that could not be open
-                    not_open.append(f"Could not open the file {os.path.relpath(name, start=user_path)}")
-                    continue
+            if os.path.isdir(name) or os.path.relpath(name, start=user_path) in scan_ignore:
+                continue
+            try:
+                with open(name, mode='r', encoding='utf-8') as f:
+                    for file_line in f:
+                        for secret_word in secret_words:
+                            if secret_word in file_line:
+                                file_list.append({'file_name': os.path.relpath(name, start=user_path),
+                                                  'file_line': file_line})
+            except Exception:
+                # creating the list of files that could not be open
+                not_open.append(f"Could not open the file {os.path.relpath(name, start=user_path)}")
+                continue
         if not file_list:
-            return "No secrets found!"
+            return "No secrets found, good job! Keep an eye on them anyway as no tool is perfect."
         else:
             return file_list
     else:
-        return "Specified path not found. Try another one."
+        return "The given path is not found. Shall we try another one?"
 
 
 if __name__ == '__main__':
